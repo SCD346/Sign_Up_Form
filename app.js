884d08530a71675ccf00fc81e3c356bc8dbb355d
steve_D <scd346@nyu.edu>
2020-09-29 08:08:05 -0700

    form validation
    added password validation

--- app.js
+++ app.js
@@ -6,7 +6,32 @@ signUpForm.addEventListener('submit', handleSignupFormSubmit);
 // callback function for signup form submit listener
 function handleSignupFormSubmit(e) {
     // prevernt default browser behaviour
-    // TODO: add link to mdn docs
     e.preventDefault();
     console.log('submit', e)
+
+    const formDataEntries = new FormData(signUpForm).entries();
+    const { email, password } = Object.fromEntries(formDataEntries);
+
+    // submit email and password to an API
 }
+
+//Password Validation
+function validatePassword(password, minlenght) {
+    if (!password) return 'Password is required';
+    if (password.length < minlenght) {
+        return `Password must be at least ${minlenght} characters long`;
+    }
+
+    const hasCapitalLetter = /[A-Z]/g;
+    if (!hasCapitalLetter.test(password)) {
+        return 'Please use at least one capital letter.'
+    }
+
+    const hasNumber = /\d/g;
+    if (!hasNumber.test(password)) {
+        return 'Please use at least one number.'
+    }
+    return '';
+}
+
+//Email Validation
